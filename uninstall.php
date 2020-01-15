53074eedbded040b4c495c5c4c1833ffb605e0d7
Wasseem Khayrattee <w@7php.com>
2020-01-15 18:05:46 +0400

    add comment for usage

--- uninstall.php
+++ uninstall.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * When coding this page, consider checklist:
+ * - This method should be static
+ * - Check if the $_REQUEST content actually is the plugin name
+ * - Run an admin referrer check to make sure it goes through authentication
+ * - Verify the output of $_GET makes sense
+ * - Repeat with other user roles. Best directly by using the links/query string parameters.
+ * - Repeat things for multisite. Once for a single site in the network, once sitewide.
+ *
+ * ref: https://github.com/DevinVinson/WordPress-Plugin-Boilerplate/blob/master/plugin-name/uninstall.php
+ */
 if (! defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
