d8494530eab0bd010c3be51773826dfdb933a951
Wasseem Khayrattee <w@7php.com>
2020-01-16 18:37:52 +0400

    use templating approach using SavantPHP for the display of meta_box in admin

---
+++ admin/views/custom_fields.tpl.php
@@ -0,0 +1,104 @@
+
+<div class="margin:10px 10px;">
+    <input type="hidden" name="tmc_box_nonce" value="<?php echo wp_create_nonce( basename(__FILE__))?>">
+
+    <div data-name="price" data-type="number" data-key="field_5e201f02223c5" data-required="1">
+        <div class="acf-label">
+            <label for="acf-field_5e201f02223c5">Price <span class="acf-required">*</span></label></div>
+        <div class="acf-input">
+            <div class="acf-input-wrap">
+                <input type="number" id="acf-field_5e201f02223c5" name="acf[field_5e201f02223c5]" step="any" required="required">
+            </div>
+        </div>
+    </div>
+    <div data-name="display_price" data-type="true_false" data-key="field_5e201f46223c6">
+        <div class="acf-label">
+            <label for="acf-field_5e201f46223c6">Display price</label></div>
+        <div class="acf-input">
+            <div class="acf-true-false">
+                <input type="hidden" name="acf[field_5e201f46223c6]" value="0">	<label>
+                    <input type="checkbox" id="acf-field_5e201f46223c6" name="acf[field_5e201f46223c6]" value="1" class="" autocomplete="off">
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div data-name="quantity" data-type="number" data-key="field_5e201f64223c7">
+        <div class="acf-label">
+            <label for="acf-field_5e201f64223c7">Quantity</label></div>
+        <div class="acf-input">
+            <div class="acf-input-wrap">
+                <input type="number" id="acf-field_5e201f64223c7" name="acf[field_5e201f64223c7]" step="any">
+            </div>
+        </div>
+    </div>
+    <div data-name="display_quantity" data-type="true_false" data-key="field_5e201f73223c8">
+        <div class="acf-label">
+            <label for="acf-field_5e201f73223c8">Display Quantity</label></div>
+        <div class="acf-input">
+            <div class="acf-true-false">
+                <input type="hidden" name="acf[field_5e201f73223c8]" value="0">	<label>
+                    <input type="checkbox" id="acf-field_5e201f73223c8" name="acf[field_5e201f73223c8]" value="1" class="" autocomplete="off">
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div data-name="stock" data-type="number" data-key="field_5e201f8c223c9">
+        <div class="acf-label">
+            <label for="acf-field_5e201f8c223c9">Amount of stock</label></div>
+        <div class="acf-input">
+            <div class="acf-input-wrap"><input type="number" id="acf-field_5e201f8c223c9" name="acf[field_5e201f8c223c9]" step="any"></div></div>
+    </div>
+
+    <div data-name="promotional_price" data-type="number" data-key="field_5e201fc1223ca">
+        <div class="acf-label">
+            <label for="acf-field_5e201fc1223ca">Promotional price</label></div>
+        <div class="acf-input">
+            <div class="acf-input-wrap"><input type="number" id="acf-field_5e201fc1223ca" name="acf[field_5e201fc1223ca]" step="any"></div></div>
+    </div>
+    <div data-name="display_promo" data-type="true_false" data-key="field_5e201fd2223cb">
+        <div class="acf-label">
+            <label for="acf-field_5e201fd2223cb">Display promo</label></div>
+        <div class="acf-input">
+            <div class="acf-true-false">
+                <input type="hidden" name="acf[field_5e201fd2223cb]" value="0">	<label>
+                    <input type="checkbox" id="acf-field_5e201fd2223cb" name="acf[field_5e201fd2223cb]" value="1" class="" autocomplete="off">
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div data-name="sales_start_date" data-type="date_time_picker" data-key="field_5e201fe5223cc">
+        <div class="acf-label">
+            <label for="acf-field_5e201fe5223cc">Sales start date</label></div>
+        <div class="acf-input">
+            <div class="acf-date-time-picker acf-input-wrap" data-date_format="dd/mm/yy" data-time_format="h:mm tt" data-first_day="1">
+                <input type="hidden" id="acf-field_5e201fe5223cc" class="input-alt" name="acf[field_5e201fe5223cc]" value="" style="cursor: pointer;">
+                <input type="text" class="input hasDatepicker" value="" id="dp1579184348532">
+            </div>
+        </div>
+    </div>
+    <div data-name="sales_end_date" data-type="date_time_picker" data-key="field_5e201ff8223cd">
+        <div class="acf-label">
+            <label for="acf-field_5e201ff8223cd">Sales end date</label></div>
+        <div class="acf-input">
+            <div class="acf-date-time-picker acf-input-wrap" data-date_format="dd/mm/yy" data-time_format="h:mm tt" data-first_day="1">
+                <input type="hidden" id="acf-field_5e201ff8223cd" class="input-alt" name="acf[field_5e201ff8223cd]" value="" style="cursor: pointer;">
+                <input type="text" class="input hasDatepicker" value="" id="dp1579184348533">
+            </div>
+        </div>
+    </div>
+
+    <div data-name="display_date" data-type="true_false" data-key="field_5e202010223ce">
+        <div class="acf-label">
+            <label for="acf-field_5e202010223ce">Display date</label></div>
+        <div class="acf-input">
+            <div class="acf-true-false">
+                <input type="hidden" name="acf[field_5e202010223ce]" value="0">	<label>
+                    <input type="checkbox" id="acf-field_5e202010223ce" name="acf[field_5e202010223ce]" value="1" class="" autocomplete="off">
+                </label>
+            </div>
+        </div>
+    </div>
+</div>
