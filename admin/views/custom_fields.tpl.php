46f795ca0b97ae9557a178998b1b0b6b367fda18
Wasseem Khayrattee <w@7php.com>
2020-01-16 23:16:16 +0400

    adjust params for dynamic display

--- admin/views/custom_fields.tpl.php
+++ admin/views/custom_fields.tpl.php
@@ -1,103 +1,98 @@
 
 <div class="margin:10px 10px;">
-    <?php wp_nonce_field('tmc_custom_box', 'tmc_custom_box_nonce'); ?>
+    <?php wp_nonce_field('tmc_custom_box_action', 'tmc_custom_box_nonce'); ?>
 
-    <div data-name="price" data-type="number" data-key="field_5e201f02223c5" data-required="1">
-        <div class="acf-label">
-            <label for="acf-field_5e201f02223c5">Price</label></div>
-        <div class="acf-input">
-            <div class="acf-input-wrap">
-                <input type="number" id="acf-field_5e201f02223c5" name="acf[field_5e201f02223c5]" step="any" required="required">
+    <div data-name="<?php echo esc_attr($this->field_list['tmc_price']['name']) ?>" data-type="number" data-key="<?php echo esc_attr($this->field_list['tmc_price']['name']) ?>">
+        <div class="label">
+            <label for="<?php echo esc_attr($this->field_list['tmc_price']['name']) ?>"><?php echo esc_attr($this->field_list['tmc_price']['label']) ?>:</label></div>
+        <div class="input">
+            <div class="input-wrap">
+                <input type="number" id="<?php echo esc_attr($this->field_list['tmc_price']['name']) ?>" name="<?php echo esc_attr($this->field_list['tmc_price']['name']) ?>" value="<?php echo esc_attr($this->field_list['tmc_price']['value']) ?>">
             </div>
         </div>
     </div>
-    <div data-name="display_price" data-type="true_false" data-key="field_5e201f46223c6">
+    <div data-name="<?php echo esc_attr($this->field_list['tmc_display_price']['name']) ?>" data-type="true_false" data-key="field_5e201f46223c6">
         <div class="acf-label">
-            <label for="acf-field_5e201f46223c6">Display price</label></div>
+            <label for="<?php echo esc_attr($this->field_list['tmc_display_price']['name']) ?>"><?php echo esc_attr($this->field_list['tmc_display_price']['label']) ?>:</label></div>
         <div class="acf-input">
             <div class="acf-true-false">
-                <input type="hidden" name="acf[field_5e201f46223c6]" value="0">	<label>
-                    <input type="checkbox" id="acf-field_5e201f46223c6" name="acf[field_5e201f46223c6]" value="1" class="" autocomplete="off">
+                    <input type="checkbox" id="<?php echo esc_attr($this->field_list['tmc_display_price']['name']) ?>" name="<?php echo esc_attr($this->field_list['tmc_display_price']['name']) ?>" value="<?php echo esc_attr($this->field_list['tmc_display_price']['value']) ?>">
                 </label>
             </div>
         </div>
     </div>
 
-    <div data-name="quantity" data-type="number" data-key="field_5e201f64223c7">
-        <div class="acf-label">
-            <label for="acf-field_5e201f64223c7">Quantity</label></div>
-        <div class="acf-input">
-            <div class="acf-input-wrap">
-                <input type="number" id="acf-field_5e201f64223c7" name="acf[field_5e201f64223c7]" step="any">
+    <div data-name="<?php echo esc_attr($this->field_list['tmc_quantity']['name']) ?>" data-type="number" data-key="<?php echo esc_attr($this->field_list['tmc_quantity']['name']) ?>">
+        <div class="label">
+            <label for="<?php echo esc_attr($this->field_list['tmc_quantity']['name']) ?>"><?php echo esc_attr($this->field_list['tmc_quantity']['label']) ?>:</label></div>
+        <div class="input">
+            <div class="input-wrap">
+                <input type="number" id="<?php echo esc_attr($this->field_list['tmc_quantity']['name']) ?>" name="<?php echo esc_attr($this->field_list['tmc_quantity']['name']) ?>" value="<?php echo esc_attr($this->field_list['tmc_quantity']['value']) ?>">
             </div>
         </div>
     </div>
-    <div data-name="display_quantity" data-type="true_false" data-key="field_5e201f73223c8">
-        <div class="acf-label">
-            <label for="acf-field_5e201f73223c8">Display Quantity</label></div>
-        <div class="acf-input">
-            <div class="acf-true-false">
-                <input type="hidden" name="acf[field_5e201f73223c8]" value="0">	<label>
-                    <input type="checkbox" id="acf-field_5e201f73223c8" name="acf[field_5e201f73223c8]" value="1" class="" autocomplete="off">
+    <div data-name="<?php echo esc_attr($this->field_list['tmc_display_quantity']['name']) ?>" data-type="true_false" data-key="<?php echo esc_attr($this->field_list['tmc_display_quantity']['name']) ?>">
+        <div class="label">
+            <label for="<?php echo esc_attr($this->field_list['tmc_display_quantity']['name']) ?>"><?php echo esc_attr($this->field_list['tmc_display_quantity']['label']) ?>:</label></div>
+        <div class="input">
+            <div class="true-false">
+                    <input type="checkbox" id="<?php echo esc_attr($this->field_list['tmc_display_quantity']['name']) ?>" name="<?php echo esc_attr($this->field_list['tmc_display_quantity']['name']) ?>" value="<?php echo esc_attr($this->field_list['tmc_display_quantity']['value']) ?>">
                 </label>
             </div>
         </div>
     </div>
 
-    <div data-name="stock" data-type="number" data-key="field_5e201f8c223c9">
-        <div class="acf-label">
-            <label for="acf-field_5e201f8c223c9">Amount of stock</label></div>
-        <div class="acf-input">
-            <div class="acf-input-wrap"><input type="number" id="acf-field_5e201f8c223c9" name="acf[field_5e201f8c223c9]" step="any"></div></div>
+    <div data-name="<?php echo esc_attr($this->field_list['tmc_stock']['name']) ?>" data-type="number" data-key="<?php echo esc_attr($this->field_list['tmc_stock']['name']) ?>">
+        <div class="label">
+            <label for="<?php echo esc_attr($this->field_list['tmc_stock']['name']) ?>"><?php echo esc_attr($this->field_list['tmc_stock']['label']) ?>:</label></div>
+        <div class="input">
+            <div class="input-wrap">
+                <input type="number" id="<?php echo esc_attr($this->field_list['tmc_stock']['name']) ?>" name="<?php echo esc_attr($this->field_list['tmc_stock']['name']) ?>" value="<?php echo esc_attr($this->field_list['tmc_stock']['value']) ?>"></div></div>
     </div>
 
-    <div data-name="promotional_price" data-type="number" data-key="field_5e201fc1223ca">
-        <div class="acf-label">
-            <label for="acf-field_5e201fc1223ca">Promotional price</label></div>
+    <div data-name="<?php echo esc_attr($this->field_list['tmc_promotional_price']['name']) ?>" data-type="number" data-key="<?php echo esc_attr($this->field_list['tmc_promotional_price']['name']) ?>a">
+        <div class="label">
+            <label for="<?php echo esc_attr($this->field_list['tmc_promotional_price']['name']) ?>"><?php echo esc_attr($this->field_list['tmc_promotional_price']['label']) ?>:</label></div>
         <div class="acf-input">
-            <div class="acf-input-wrap"><input type="number" id="acf-field_5e201fc1223ca" name="acf[field_5e201fc1223ca]" step="any"></div></div>
+            <div class="input-wrap">
+                <input type="number" id="<?php echo esc_attr($this->field_list['tmc_promotional_price']['name']) ?>" name="<?php echo esc_attr($this->field_list['tmc_promotional_price']['name']) ?>" value="<?php echo esc_attr($this->field_list['tmc_promotional_price']['value']) ?>"></div></div>
     </div>
-    <div data-name="display_promo" data-type="true_false" data-key="field_5e201fd2223cb">
-        <div class="acf-label">
-            <label for="acf-field_5e201fd2223cb">Display promo</label></div>
-        <div class="acf-input">
-            <div class="acf-true-false">
-                <input type="hidden" name="acf[field_5e201fd2223cb]" value="0">	<label>
-                    <input type="checkbox" id="acf-field_5e201fd2223cb" name="acf[field_5e201fd2223cb]" value="1" class="" autocomplete="off">
+    <div data-name="<?php echo esc_attr($this->field_list['tmc_display_promo']['name']) ?>" data-type="true_false" data-key="<?php echo esc_attr($this->field_list['tmc_display_promo']['name']) ?>">
+        <div class="label">
+            <label for="<?php echo esc_attr($this->field_list['tmc_display_promo']['name']) ?>"><?php echo esc_attr($this->field_list['tmc_display_promo']['label']) ?>:</label></div>
+        <div class="input">
+            <div class="true-false">
+                    <input type="checkbox" id="acf-<?php echo esc_attr($this->field_list['tmc_display_promo']['name']) ?>" name="<?php echo esc_attr($this->field_list['tmc_display_promo']['name']) ?>" value="<?php echo esc_attr($this->field_list['tmc_display_promo']['value']) ?>">
                 </label>
             </div>
         </div>
     </div>
 
-    <div data-name="sales_start_date" data-type="date_time_picker" data-key="field_5e201fe5223cc">
-        <div class="acf-label">
-            <label for="acf-field_5e201fe5223cc">Sales start date</label></div>
-        <div class="acf-input">
-            <div class="acf-date-time-picker acf-input-wrap" data-date_format="dd/mm/yy" data-time_format="h:mm tt" data-first_day="1">
-                <input type="hidden" id="acf-field_5e201fe5223cc" class="input-alt" name="acf[field_5e201fe5223cc]" value="" style="cursor: pointer;">
-                <input type="text" class="input hasDatepicker" value="" id="dp1579184348532">
+    <div data-name="<?php echo esc_attr($this->field_list['tmc_sales_start_date']['name']) ?>" data-type="date_time_picker" data-key="<?php echo esc_attr($this->field_list['tmc_sales_start_date']['name']) ?>">
+        <div class="label">
+            <label for="<?php echo esc_attr($this->field_list['tmc_sales_start_date']['name']) ?>"><?php echo esc_attr($this->field_list['tmc_sales_start_date']['label']) ?>:</label></div>
+        <div class="input">
+            <div class="date-time-picker input-wrap" data-date_format="dd/mm/yy" data-time_format="h:mm tt" data-first_day="1">
+                <input type="text" class="input hasDatepicker" value="<?php echo esc_attr($this->field_list['tmc_sales_start_date']['value']) ?>" id="<?php echo esc_attr($this->field_list['tmc_sales_start_date']['name']) ?>" name="<?php echo esc_attr($this->field_list['tmc_sales_start_date']['name']) ?>">
             </div>
         </div>
     </div>
-    <div data-name="sales_end_date" data-type="date_time_picker" data-key="field_5e201ff8223cd">
-        <div class="acf-label">
-            <label for="acf-field_5e201ff8223cd">Sales end date</label></div>
-        <div class="acf-input">
-            <div class="acf-date-time-picker acf-input-wrap" data-date_format="dd/mm/yy" data-time_format="h:mm tt" data-first_day="1">
-                <input type="hidden" id="acf-field_5e201ff8223cd" class="input-alt" name="acf[field_5e201ff8223cd]" value="" style="cursor: pointer;">
-                <input type="text" class="input hasDatepicker" value="" id="dp1579184348533">
+    <div data-name="<?php echo esc_attr($this->field_list['tmc_sales_end_date']['name']) ?>" data-type="date_time_picker" data-key="<?php echo esc_attr($this->field_list['tmc_sales_end_date']['name']) ?>">
+        <div class="label">
+            <label for="<?php echo esc_attr($this->field_list['tmc_sales_end_date']['name']) ?>"><?php echo esc_attr($this->field_list['tmc_sales_end_date']['label']) ?>:</label></div>
+        <div class="input">
+            <div class="date-time-picker input-wrap" data-date_format="dd/mm/yy" data-time_format="h:mm tt" data-first_day="1">
+                <input type="text" class="input hasDatepicker" value="<?php echo esc_attr($this->field_list['tmc_sales_end_date']['value']) ?>" id="<?php echo esc_attr($this->field_list['tmc_sales_end_date']['name']) ?>" name="<?php echo esc_attr($this->field_list['tmc_sales_end_date']['name']) ?>">
             </div>
         </div>
     </div>
 
-    <div data-name="display_date" data-type="true_false" data-key="field_5e202010223ce">
-        <div class="acf-label">
-            <label for="acf-field_5e202010223ce">Display date</label></div>
-        <div class="acf-input">
-            <div class="acf-true-false">
-                <input type="hidden" name="acf[field_5e202010223ce]" value="0">	<label>
-                    <input type="checkbox" id="acf-field_5e202010223ce" name="acf[field_5e202010223ce]" value="1" class="" autocomplete="off">
-                </label>
+    <div data-name="<?php echo esc_attr($this->field_list['tmc_display_date']['name']) ?>" data-type="true_false" data-key="<?php echo esc_attr($this->field_list['tmc_display_date']['name']) ?>">
+        <div class="label">
+            <label for="<?php echo esc_attr($this->field_list['tmc_display_date']['name']) ?>"><?php echo esc_attr($this->field_list['tmc_display_date']['label']) ?>:</label></div>
+        <div class="input">
+            <div class="true-false">
+                    <input type="checkbox" id="<?php echo esc_attr($this->field_list['tmc_display_date']['name']) ?>" name="<?php echo esc_attr($this->field_list['tmc_display_date']['name']) ?>" value="<?php echo esc_attr($this->field_list['tmc_display_date']['value']) ?>">
             </div>
         </div>
     </div>
