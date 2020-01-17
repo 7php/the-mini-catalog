fe747b26401df5589142dce79a979fa28c1cda2c
Wasseem Khayrattee <w@7php.com>
2020-01-18 01:30:05 +0400

    remove unused space in indentation

--- admin/views/permalink-settings.tpl.php
+++ admin/views/permalink-settings.tpl.php
@@ -7,15 +7,19 @@ use TMC_MiniCatalog\PostTypeEnum;
 <table class="form-table tmc-permalink-structure">
     <tbody>
         <tr>
-            <th><label><input name="product_permalink" type="radio" value="<?php echo esc_attr( $this->structures[0] ); ?>" class="wctog" <?php checked( $this->structures[0], $this->permalinks['product_base'] ); ?> /> <?php esc_html_e( 'Default', PostTypeEnum::CUSTOM_TEXT_DOMAIN ); ?></label></th>
+            <th>
+                <label>
+                    <input name="product_permalink" type="radio" value="<?php echo esc_attr( $this->structures[0] ); ?>" class="wctog" <?php checked( $this->structures[0], $this->permalinks['product_base'] ); ?> /> <?php esc_html_e( 'Default', PostTypeEnum::CUSTOM_TEXT_DOMAIN ); ?>
+                </label>
+            </th>
             <td><code class="default-example"><?php echo esc_html( home_url() ); ?>/?product=sample-product</code> | with friendly SLUG: <code class="non-default-example"><?php echo esc_html( home_url() ); ?>/<?php echo esc_html( $this->product_base ); ?>/sample-product/</code></td>
         </tr>
 
         <tr>
             <th>
                 <label>
-                    <input name="product_permalink" id="tmc_custom_selection" type="radio" value="custom" class="tog" <?php checked( in_array( $this->permalinks['product_base'], $this->structures, true ), false ); ?> />
-                    <?php esc_html_e( 'Custom base', PostTypeEnum::CUSTOM_TEXT_DOMAIN ); ?>
+                    <input name="product_permalink" id="tmc_custom_selection" type="radio" value="custom" class="tog" <?php checked( in_array($this->permalinks['product_base'], $this->structures, true), false); ?> />
+                    <?php esc_html_e('Custom base', PostTypeEnum::CUSTOM_TEXT_DOMAIN ); ?>
                 </label>
             </th>
             <td>
