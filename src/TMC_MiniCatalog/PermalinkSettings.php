7d6ca98ff20410821a674ecdaa913ce177e27e6c
Wasseem Khayrattee <w@7php.com>
2020-01-18 01:32:56 +0400

    completed handling of custom permalink through Permalink Settings

--- src/TMC_MiniCatalog/PermalinkSettings.php
+++ src/TMC_MiniCatalog/PermalinkSettings.php
@@ -16,6 +16,11 @@ class PermalinkSettings
     private function __clone() { }
 
     /**
+     * Private to prevent direct instantiation
+     */
+    private function __construct() {}
+
+    /**
      * @return PermalinkSettings|null
      */
     public static function getInstance()
@@ -26,17 +31,67 @@ class PermalinkSettings
         return self::$_instance;
     }
 
-    private function __construct() {}
-
+    /**
+     *
+     */
     public function initPermalinks()
     {
         $this->settings_init();
+        $this->save_settings();
     }
 
+    /**
+     * Display the custom section on Permalink Settings Page
+     */
     public function settings_init()
     {
         add_settings_section('tmc-permalink', 'TMC Product permalinks', [$this, 'settings'], 'permalink');
         $this->permalinks = $this->tmc_get_permalink_structure();
+    }
+
+    /**
+     * prepping TPL for custom Permalink Settings Section
+     */
+    public function settings()
+    {
+        $tpl               = tplObject();
+        $tpl->permalinks   = $this->permalinks;
+        $tpl->product_base =  $product_base = PostTypeEnum::REWRITE_SLUG;
+        $tpl->structures   = [0 => '', ];
+
+        $tpl->display('permalink-settings.tpl.php');
+    }
+
+    /**
+     * On Submit on page Permalink Settings, SAVE entry
+     */
+    public function save_settings()
+    {
+        if (! is_admin()) {
+            return;
+        }
+
+        /**
+         * Save the options; settings api does not trigger save for the permalinks page.
+         */
+        if (isset($_POST['permalink_structure'], $_POST['tmc-permalinks-nonce']) && wp_verify_nonce(wp_unslash( $_POST['tmc-permalinks-nonce']), 'tmc-permalinks') ) {
+
+            $permalinks     = (array) get_option(PostTypeEnum::TMC_PERMALINKS_KEY, []);
+            $product_base   = isset($_POST['product_permalink']) ? sanitize_text_field(wp_unslash( $_POST['product_permalink']) ) : '';
+
+            if ('custom' === $product_base) {
+                if (isset( $_POST['product_permalink_structure']) ) {
+                    $product_base = preg_replace( '#/+#', '/', '/' . str_replace( '#', '', trim(wp_unslash($_POST['product_permalink_structure']))) );
+                } else {
+                    $product_base = '/';
+                }
+            } else {
+                $product_base = 'product';
+            }
+
+            $permalinks['product_base'] = sanitize_title($product_base);
+            update_option(PostTypeEnum::TMC_PERMALINKS_KEY, $permalinks);
+        }
     }
 
     /**
@@ -54,15 +109,5 @@ class PermalinkSettings
         $permalinks[PostTypeEnum::TMC_REWRITE_SLUG] = untrailingslashit($permalinks['product_base']);
 
         return $permalinks;
-    }
-
-    public function settings()
-    {
-        $tpl               = tplObject();
-        $tpl->permalinks   = $this->permalinks;
-        $tpl->product_base =  $product_base = PostTypeEnum::REWRITE_SLUG;
-        $tpl->structures   = [0 => '', ];
-
-        $tpl->display('permalink-settings.tpl.php');
     }
 }
