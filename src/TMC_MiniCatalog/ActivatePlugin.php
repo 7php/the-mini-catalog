26dc4f6f520251fab97d92b8ffe17482ef70e590
Wasseem Khayrattee <w@7php.com>
2020-01-20 22:45:29 +0400

    during activation - create some pages programmatically

--- src/TMC_MiniCatalog/ActivatePlugin.php
+++ src/TMC_MiniCatalog/ActivatePlugin.php
@@ -18,7 +18,39 @@ class ActivatePlugin
          * ref: https://developer.wordpress.org/reference/functions/register_post_type/#flushing-rewrite-on-activation
          */
         update_option(PostTypeEnum::TMC_PERMALINKS_KEY, ['product_base' => 'product']);
+        $this->createPageStore();
+        $this->createPagePromo();
         initCustomPostType();
         flush_rewrite_rules();
+    }
+
+    /**
+     * To create PAGE Store
+     */
+    public function createPageStore()
+    {
+        $page = [
+            'post_title'    => wp_strip_all_tags('Store'),
+            'post_content'  => 'the store page to display list of products',
+            'post_status'   => 'publish',
+            'post_author'   => 1,
+            'post_type'     => 'page'
+        ];
+        wp_insert_post($page);
+    }
+
+    /**
+     * TO create PAGE Mass Promo
+     */
+    public function createPagePromo()
+    {
+        $page = [
+            'post_title'    => wp_strip_all_tags('Mass Promo'),
+            'post_content'  => 'List all promo products',
+            'post_status'   => 'publish',
+            'post_author'   => 1,
+            'post_type'     => 'page'
+        ];
+        wp_insert_post($page);
     }
 }
