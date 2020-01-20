7e651163ada674831b3de5e1b80292bc4c1c1890
Wasseem Khayrattee <w@7php.com>
2020-01-20 22:46:07 +0400

    during deactivation - delete initially created  pages programmatically

--- src/TMC_MiniCatalog/DeActivatePlugin.php
+++ src/TMC_MiniCatalog/DeActivatePlugin.php
@@ -18,6 +18,30 @@ class DeActivatePlugin
     public function __construct()
     {
         delete_option(PostTypeEnum::TMC_PERMALINKS_KEY);
+        $this->deletePageStore();
+        $this->deletePagePromo();
         flush_rewrite_rules();
+    }
+
+    /**
+     * To delete PAGE Store
+     */
+    public function deletePageStore()
+    {
+        $page_title = [
+            'post_title' => wp_strip_all_tags('Store'),
+        ];
+        wp_delete_post(get_page_by_title($page_title['post_title'])->ID);
+    }
+
+    /**
+     * TO delete PAGE Mass Promo
+     */
+    public function deletePagePromo()
+    {
+        $page_title = [
+            'post_title' => wp_strip_all_tags('Mass Promo'),
+        ];
+        wp_delete_post(get_page_by_title($page_title['post_title'])->ID);
     }
 }
